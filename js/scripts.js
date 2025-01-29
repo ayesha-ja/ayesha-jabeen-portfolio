23201c528a8ba492a5687bfc00f4c19224cfe8a7
ayesha-ja <128830423+ayesha-ja@users.noreply.github.com>
2025-01-29 14:04:18 +0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(data => document.getElementById('navbar').innerHTML = data)
+  .catch(error => console.error('Error loading navbar:', error));
+
+  fetch('footer.html')
+  .then(response => response.text())
+  .then(data => document.getElementById('footer').innerHTML = data)
+  .catch(error =>console.error('Error loading Footer:' , error))
