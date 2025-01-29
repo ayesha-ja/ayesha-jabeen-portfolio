3b97ad5914db1b76548b2f5ba794e3cf0fe4800f
ayesha-ja <128830423+ayesha-ja@users.noreply.github.com>
2025-01-29 16:08:35 +0500

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,30 @@ fetch('navbar.html')
   .then(response => response.text())
   .then(data => document.getElementById('footer').innerHTML = data)
   .catch(error =>console.error('Error loading Footer:' , error))
+
+  
+          //Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
